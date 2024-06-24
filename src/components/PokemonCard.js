66cc016ad16c0b5ae6d183c7a3171da8bcbfd15d
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-24 15:39:45 -0400

    Click to show back image

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,15 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import { Card } from "semantic-ui-react"
 
 function PokemonCard({ pokemon }) {
   const { id, name, hp, sprites } = pokemon
   const { front, back } = sprites
+  const [isFront, setIsFront] = useState(true)
+
+  function handleImageClick() {
+    setIsFront(!isFront)
+  }
 
   return (
     <Card>
       <div>
         <div className="image">
-          <img src={front} alt="oh no!" />
+          <img
+            src={isFront ? front : back}
+            alt="oh no!"
+            onClick={handleImageClick}
+          />
         </div>
         <div className="content">
           <div className="header">{name}</div>
