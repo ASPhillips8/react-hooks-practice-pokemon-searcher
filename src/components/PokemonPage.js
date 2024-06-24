2ef6a6899e208025dc543f667f7a20f1978b1066
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-24 15:33:26 -0400

    Displayed pokemon

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,18 @@
-import React from "react";
-import PokemonCollection from "./PokemonCollection";
-import PokemonForm from "./PokemonForm";
-import Search from "./Search";
-import { Container } from "semantic-ui-react";
+import React, { useEffect, useState } from "react"
+import PokemonCollection from "./PokemonCollection"
+import PokemonForm from "./PokemonForm"
+import Search from "./Search"
+import { Container } from "semantic-ui-react"
 
 function PokemonPage() {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+      .then((response) => response.json())
+      .then((pokemonData) => setPokemons(pokemonData))
+  }, [])
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,9 +21,9 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={pokemons} />
     </Container>
-  );
+  )
 }
 
-export default PokemonPage;
+export default PokemonPage
