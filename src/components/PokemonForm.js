446daa516c91232cd8a1e580de26b1670e12368f
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-25 10:28:48 -0400

    Create new object with submit

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -2,28 +2,56 @@ import React, { useState } from "react"
 import { Form } from "semantic-ui-react"
 
 function PokemonForm() {
+  const [name, setName] = useState("")
+  const [hp, setHp] = useState("")
+  const [frontUrl, setFrontUrl] = useState("")
+  const [backUrl, setBackUrl] = useState("")
+
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    const formData = {
+      name: name,
+      hp: hp,
+      sprites: {
+        front: frontUrl,
+        back: backUrl,
+      },
+    }
+    console.log(formData)
+  }
+
   return (
     <div>
       <h3>Add a Pokemon!</h3>
-      <Form
-        onSubmit={() => {
-          console.log("submitting form...")
-        }}
-      >
+      <Form onSubmit={handleFormSubmit}>
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input
+            fluid
+            label="Name"
+            placeholder="Name"
+            name="name"
+            onChange={(event) => setName(event.target.value)}
+          />
+          <Form.Input
+            fluid
+            label="hp"
+            placeholder="hp"
+            name="hp"
+            onChange={(event) => setHp(event.target.value)}
+          />
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            onChange={(event) => setFrontUrl(event.target.value)}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            onChange={(event) => setBackUrl(event.target.value)}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
